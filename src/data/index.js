8a33d1103bc3ffbdf74c340447b30eacafbe14fa
Enzo CECILLON <enzocecillon@macbook-pro-de-enzo.home>
2022-12-25 10:41:42 +0100

    chore: move data file and init skill file with my prev evalutation

---
+++ src/data/index.js
@@ -0,0 +1,9 @@
+import skills from './skills';
+import temperatures from './temperatures';
+
+const Data = {
+  skills,
+  temperatures
+};
+
+export default Data;
--- src/data/temperatures.js
+++ src/data/temperatures.js
@@ -1,7 +1,9 @@
-export const data = [
+const temperatures = [
   { name: 'Janvier', temperature: 7 },
   { name: 'Février', temperature: 6 },
   { name: 'Mars', temperature: 8 },
   { name: 'Avril', temperature: 10 },
   { name: 'Mai', temperature: 12 },
 ];
+
+export default temperatures;
