2e9b1a1dadb22e5732b12363ad66514755e22fd9
Enzo CECILLON <enzocecillon@macbook-pro-de-enzo.home>
2022-12-25 10:42:42 +0100

    feat: define own light and dark theme provider

---
+++ src/ui/components/theme/theme-provider.js
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from 'react';
+
+export const ThemeContext = createContext();
+
+export function ThemeProvider(props) {
+  const [theme, setTheme] = useState('dark');
+
+  function toggleTheme() {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
+}
